8966138d250d5ed8ad9ebe88a9f90aa855013d18
italobarzola <italo.barzola@links.com.ec>
2022-10-08 22:01:13 -0500

    Product class save

--- src/Contexts/Shared/infrastructure/persistence/firebase/FirebaseRepository.ts
+++ src/Contexts/Shared/infrastructure/persistence/firebase/FirebaseRepository.ts
@@ -15,6 +15,7 @@ export abstract class FirebaseRepository<T extends AggregateRoot>{
         const collection = this.collection().doc(aggregateRoot.toPrimitives().id);
         const document = { ...aggregateRoot.toPrimitives() };
         return await collection.set(document);
+
     }
 
     protected async paginatePaginateQuery<T>(limit: number, startAfter: number): Promise<Array<T>> {
