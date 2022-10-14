f39656a99cd9f58037711f044e2befa7aa79979e
italobarzola <italo.barzola@links.com.ec>
2022-10-14 17:16:14 -0500

    Refactor app sales

--- src/apps/Sales/Authentication/routes/authentication.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-import container from '../../dependency-injection';
-import { AuthenticationSignInController } from '../controllers/AuthenticationSignInController';
-
-export const register = (router: Router) => {
-
-    const signInController = container.get<AuthenticationSignInController>('Sales.authentication.AuthenticationSignInController');
-    router.post("/api/auth/create-cookie-session", (req: Request, res: Response) => signInController.run(req, res));
-}
--- src/apps/Sales/Subscribers/routes/subscribers.route.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-import container from '../../dependency-injection';
-import { SubscriberSaveController } from '../controllers/SubscriberSaveController';
-
-export const register = (router: Router) => {
-
-    const saveController = container.get<SubscriberSaveController>('Sales.subscribers.SubscriberSaveController');
-    router.post("/api/subscribers", (req: Request, res: Response) => saveController.run(req, res));
-}
